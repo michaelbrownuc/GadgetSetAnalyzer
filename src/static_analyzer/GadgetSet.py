a9bcb4c9ee594f19ffabc8ffd9dfd4364146e254
Brown, Michael D <Michael.Brown@gtri.gatech.edu>
2021-03-08 17:01:53 -0500

    #6 WIP Added lots of gadget classification code.

--- src/static_analyzer/GadgetSet.py
+++ src/static_analyzer/GadgetSet.py
@@ -10,6 +10,7 @@ import angr
 
 # Local Imports
 from static_analyzer.Gadget import Gadget
+from static_analyzer.Instruction import Instruction
 
 
 class GadgetSet(object):
@@ -82,7 +83,21 @@ class GadgetSet(object):
 
 
         # Scan ROP gadgets to determine set expressivity
-        # TODO
+        self.practical_ROP = [False for i in range(11)]
+        self.practical_ASLR_ROP = [False for i in range(35)]
+        self.turing_complete_ROP = [False for i in range(17)]
+        quality_threshold = 4.0
+
+        for gadget in self.ROPGadgets:
+            if gadget.score <= quality_threshold:
+                self.classify_gadget(gadget)
+
+        # Corner cases:
+        # 1 If practical ROP class 4 (index 3) not satisfied by a ROP gadget, a JOP gadget targeting [rax] or [rdi] will do TODO
+        # 2 If practical ROP class 6 (index 5) not satisfied by a ROP gadget, a JOP gadget targeting rax, [rax], rdi, or [rdi] will do TODO
+        # 3 If practical ROP class 8 (index 7) not satisfied by a ROP Gadget, a a JOP gadget targeting [rax], [rdi], [rsi] will do TODO
+        # 4 Practical ASLR ROP class 1 (index 0) can be satisfied with jumps to GPRs TODO
+        # 4 Practical ASLR ROP class 2 (index 1) can be satisfied with jumps to GPRs TODO
 
         # TODO Add an optional summary print of counts rejected, duplicate, scores etc.  Can recover some of this from github, previous commits
 
@@ -230,3 +245,358 @@ class GadgetSet(object):
             return rop_function
         else:
             return None
+
+    def classify_gadget(self, gadget):
+        """
+        Analyzes a gadget to determine which expressivity classes it satisfies
+        :param Gadget gadget: gadget to analyze
+        :return: None, but modifies Gadget expressivity collections
+        """
+        first_instr = gadget.instructions[0]
+        opcode = first_instr.opcode
+        op1 = first_instr.op1
+        op2 = first_instr.op2
+        op1_family = Instruction.get_operand_register_family(op1)
+        op2_family = Instruction.get_operand_register_family(op2)
+
+        # TODO DELET THIS, TESTING ONLY, RESETS satisfaction array
+        #self.practical_ROP = [False for i in range(11)]
+        #self.turing_complete_ROP = [False for i in range(17)]
+        #self.practical_ASLR_ROP = [False for i in range(35)]
+
+        # TODO: For performance, iterate through the expressivity classes and perform analysis. Analysis rules should
+        #  set as many cases as possible.
+        if self.practical_ROP[0] is False:
+            if opcode == "dec" and op1_family in [0, 5] and "[" not in op1:
+                self.practical_ROP[0] = True
+
+                # Also satisfies:
+                self.turing_complete_ROP[0] = True
+                self.practical_ASLR_ROP[9] = True
+
+        if self.practical_ROP[1] is False:
+            if opcode == "inc" and op1_family in [0, 5] and "[" not in op1:
+                self.practical_ROP[1] = True
+
+                # Also satisfies:
+                self.turing_complete_ROP[0] = True
+                self.practical_ASLR_ROP[8] = True
+
+        if self.practical_ROP[2] is False:
+            if opcode == "pop" and op1_family in [0, 5] and "[" not in op1:
+                self.practical_ROP[2] = True
+
+                # Also satisfies:
+                self.turing_complete_ROP[1] = True
+                self.practical_ASLR_ROP[5] = True
+
+        if self.practical_ROP[3] is False:
+            if (opcode == "pop" and op1_family == 4 and "[" not in op1) or \
+               (opcode in ["xchg", "move"] and op1_family == 4 and op2_family in [0, 5]
+                                                               and "[" not in op1 and "[" not in op2) or \
+               (opcode == "lea" and op1_family == 4 and op2_family in [0, 5]
+                                                    and "+" not in op2 and "-" not in op2 and "*" not in op2) or \
+               (opcode == "xchg" and op1_family in [0, 5] and op2_family == 4 and "[" not in op1 and "[" not in op2):
+                self.practical_ROP[3] = True
+
+        if self.practical_ROP[4] is False:
+            if opcode == "xchg" and ((op1_family == 0 and op2_family == 5) or (op2_family == 0 and op1_family == 5)) \
+               and "[" not in op1 and "[" not in op2:
+                self.practical_ROP[4] = True
+
+        if self.practical_ROP[5] is False:
+            if opcode == "push" and op1_family in [0, 4, 5] and "[" not in op1:
+                self.practical_ROP[5] = True
+
+        if self.practical_ROP[6] is False:
+            if opcode in ["clc", "sahf"] or \
+               (opcode in ["test", "add", "adc", "sub", "sbb", "and", "or", "xor", "cmp"] and
+               op1_family in [0, 4, 5] and op2_family in [0, 4, 5] and "[" not in op1 and "[" not in op2):
+                self.practical_ROP[6] = True
+
+                # Also satisfies:
+                self.turing_complete_ROP[7] = True
+                self.practical_ASLR_ROP[4] = True
+
+        if self.practical_ROP[7] is False:
+            if (opcode.startswith("stos") or opcode in ["mov", "add", "or"]) and "[" in op1 and "+" not in op1 and \
+               "-" not in op1 and "*" not in op1 and op1_family in [0, 4, 5] and op2_family in [0, 4, 5] and \
+               op1_family != op2_family:
+                self.practical_ROP[7] = True
+
+                # Also satisfies:
+                self.turing_complete_ROP[6] = True
+                self.practical_ASLR_ROP[2] = True
+
+        if self.practical_ROP[8] is False:
+            if (opcode.startswith("lods") or opcode in ["mov", "add", "adc", "sub", "sbb", "and", "or", "xor"]) and \
+               "[" in op2 and "+" not in op2 and "-" not in op2 and "*" not in op2 and op1_family in [0, 4, 5] and \
+               op2_family in [0, 4, 5] and op1_family != op2_family:
+                self.practical_ROP[8] = True
+
+                # Also satisfies:
+                self.turing_complete_ROP[5] = True
+                self.practical_ASLR_ROP[1] = True
+
+        # NOTE: Single rule for two classes
+        if self.practical_ROP[9] is False or self.practical_ASLR_ROP[7] is False:
+            if opcode == "leave":
+                self.practical_ROP[9] = True
+                self.practical_ASLR_ROP[7] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class LEAVE")
+
+        if self.practical_ROP[10] is False:
+            if (opcode == "pop" and op1_family == 6 and "[" not in op1) or \
+               (opcode == "xchg" and op1_family is not None and op2_family is not None and op1_family != op2_family
+                                 and (op1_family == 6 or op2_family == 6) and "[" not in op1 and "[" not in op2) or \
+               (opcode in ["add", "adc", "sub", "sbb"] and "[" not in op1 and op1_family == 6 and
+               op2_family not in [None, 6] and "+" not in op2 and "-" not in op2 and "*" not in op2):
+                self.practical_ROP[10] = True
+
+        if self.turing_complete_ROP[0] is False:
+            if opcode in ["inc", "dec"] and op1_family not in [None, 7] and "+" not in op1 and "-" not in op1 and \
+               "*" not in op1:
+                self.turing_complete_ROP[0] = True
+
+        if self.turing_complete_ROP[1] is False:
+            if opcode == "pop" and op1_family not in [None, 7] and "[" not in op1:
+                self.turing_complete_ROP[1] = True
+
+        if self.turing_complete_ROP[2] is False:
+            if opcode in ["add", "adc", "sub", "sbb"] and op1_family not in [None, 7] and "+" not in op1 and \
+                    "-" not in op1 and "*" not in op1 and op2_family not in [None, 7] and "+" not in op2 and \
+                    "-" not in op2 and "*" not in op2 and op1_family != op2_family:
+                self.turing_complete_ROP[2] = True
+
+        if self.turing_complete_ROP[3] is False:
+            if (opcode == "xor" and op1_family not in [None, 7] and "+" not in op1 and "-" not in op1 and "*" not in op1
+               and op2_family not in [None, 7] and "+" not in op2 and "-" not in op2 and "*" not in op2
+               and op1_family != op2_family) or \
+               (opcode in ["neg", "not"] and op1_family not in [None, 7] and "+" not in op1 and "-" not in op1
+               and "*" not in op1):
+                self.turing_complete_ROP[3] = True
+
+        if self.turing_complete_ROP[4] is False:
+            if opcode in ["and", "or"] and op1_family not in [None, 7] and "+" not in op1 and \
+                    "-" not in op1 and "*" not in op1 and op2_family not in [None, 7] and "+" not in op2 and \
+                    "-" not in op2 and "*" not in op2 and op1_family != op2_family:
+                self.turing_complete_ROP[4] = True
+
+        if self.turing_complete_ROP[5] is False:
+            if (opcode.startswith("lods") or opcode in ["mov", "add", "adc", "sub", "sbb", "and", "or", "xor"]) and \
+               "[" in op2 and "+" not in op2 and "-" not in op2 and "*" not in op2 and \
+               op1_family not in [None, 7] and op2_family not in [None, 7] and op1_family != op2_family:
+                self.turing_complete_ROP[5] = True
+
+        if self.turing_complete_ROP[6] is False:
+            if (opcode.startswith("stos") or opcode in ["mov", "add", "or"]) and "[" in op1 and "+" not in op1 and \
+               "-" not in op1 and "*" not in op1 and op1_family not in [None, 7] and op2_family not in [None, 7] and \
+               op1_family != op2_family:
+                self.turing_complete_ROP[6] = True
+
+        if self.turing_complete_ROP[7] is False:
+            if opcode in ["clc", "sahf"] or \
+               (opcode in ["test", "add", "adc", "sub", "sbb", "and", "or", "xor", "cmp"] and
+               op1_family not in [None, 7] and op2_family not in [None, 7] and "+" not in op1 and "-" not in op1 and
+               "*" not in op1 and "+" not in op2 and "-" not in op2 and "*" not in op2 and op1_family != op2_family):
+                self.turing_complete_ROP[7] = True
+
+        if self.turing_complete_ROP[8] is False:
+            if opcode in ["add", "adc", "sub", "sbb"] and "[" not in op2 and op2_family == 7 and \
+               op1_family not in [None, 7] and "+" not in op1 and "-" not in op1 and "*" not in op1:
+                self.turing_complete_ROP[8] = True
+
+        if self.turing_complete_ROP[9] is False:
+            if (opcode == "pop" and op1_family == 7 and "[" not in op1) or \
+               (opcode == "xchg" and op1_family is not None and op2_family is not None and op1_family != op2_family
+                                 and (op1_family == 7 or op2_family == 7) and "[" not in op1 and "[" not in op2) or \
+               (opcode in ["add", "adc", "sub", "sbb"] and "[" not in op1 and op1_family == 7 and
+               op2_family not in [None, 7] and "+" not in op2 and "-" not in op2 and "*" not in op2):
+                self.turing_complete_ROP[9] = True
+
+        if self.turing_complete_ROP[10] is False:
+            if opcode in ["lahf", "pushf"] or \
+               (opcode in ["adc", "sbb"] and op1_family not in [None, 7] and op2_family not in [None, 7] and
+               "+" not in op1 and "-" not in op1 and "*" not in op1 and
+               "+" not in op2 and "-" not in op2 and "*" not in op2 and op1_family != op2_family):
+                self.turing_complete_ROP[10] = True
+
+        # Next 6 classes have common and very specific requirements, check once
+        if opcode == "xchg" and "[" not in op1 and "[" not in op2 and op1_family != op2_family:
+            if self.turing_complete_ROP[11] is False:
+                if op1_family in [0, 1] and op2_family in [0, 1]:
+                    self.turing_complete_ROP[11] = True
+
+            if self.turing_complete_ROP[12] is False:
+                if op1_family in [0, 2] and op2_family in [0, 2]:
+                    self.turing_complete_ROP[12] = True
+
+            if self.turing_complete_ROP[13] is False:
+                if op1_family in [0, 3] and op2_family in [0, 3]:
+                    self.turing_complete_ROP[13] = True
+
+            if self.turing_complete_ROP[14] is False:
+                if op1_family in [0, 6] and op2_family in [0, 6]:
+                    self.turing_complete_ROP[14] = True
+
+            if self.turing_complete_ROP[15] is False:
+                if op1_family in [0, 4] and op2_family in [0, 4]:
+                    self.turing_complete_ROP[15] = True
+
+            if self.turing_complete_ROP[16] is False:
+                if op1_family in [0, 5] and op2_family in [0, 5]:
+                    self.turing_complete_ROP[16] = True
+
+        if self.practical_ASLR_ROP[0] is False:
+            if opcode == "push" and op1_family not in [None, 6, 7] and "[" not in op1:
+                self.practical_ASLR_ROP[0] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class JMP/PSH <GPR>")
+
+        if self.practical_ASLR_ROP[1] is False:
+            if (opcode.startswith("lods") or opcode in ["mov", "add", "adc", "sub", "sbb", "and", "or", "xor"]) and \
+               "[" in op2 and "+" not in op2 and "-" not in op2 and "*" not in op2 and \
+               op1_family not in [None, 7] and op2_family not in [None, 6, 7] and op1_family != op2_family:
+                self.practical_ASLR_ROP[1] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class LOAD <GPR> [<GPR>]")
+
+        if self.practical_ASLR_ROP[2] is False:
+            if (opcode.startswith("stos") or opcode == "mov") and "[" in op1 and "+" not in op1 and \
+               "-" not in op1 and "*" not in op1 and op1_family not in [None, 7] and op2_family not in [None, 7] and \
+               op1_family != op2_family:
+                self.practical_ASLR_ROP[2] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class STORE [<GPR>] <GPR>")
+
+        if self.practical_ASLR_ROP[3] is False:
+            #TODO ESP LOAD
+            pass
+
+        if self.practical_ASLR_ROP[4] is False:
+            if opcode in ["clc", "sahf"] or \
+               (opcode in ["test", "add", "adc", "sub", "sbb", "and", "or", "xor", "cmp"] and
+               op1_family not in [None, 7] and op2_family not in [None, 7] and "[" not in op1 and "[" not in op2):
+                self.practical_ASLR_ROP[4] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class CLEAR FLAGS")
+
+        if self.practical_ASLR_ROP[5] is False:
+            if opcode == "pop" and op1_family in [0, 4, 5] and "[" not in op1:
+                self.practical_ASLR_ROP[5] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class POP AX/SI/DI")
+
+        if self.practical_ASLR_ROP[6] is False:
+            if opcode == "pop" and op1_family in [1, 2, 3, 6] and "[" not in op1:
+                self.practical_ASLR_ROP[5] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class POP CX/CX/DX/BP")
+
+        # NOTE class 8 (index 7) is combined above
+
+        if self.practical_ASLR_ROP[8] is False:
+            if opcode == "inc" and op1_family not in [None, 7] and "[" not in op1:
+                self.practical_ASLR_ROP[8] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class INC <GPR>")
+
+        if self.practical_ASLR_ROP[9] is False:
+            if opcode == "dec" and op1_family not in [None, 7] and "[" not in op1:
+                self.practical_ASLR_ROP[9] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class DEC <GPR>")
+
+        if self.practical_ASLR_ROP[10] is False:
+            if opcode in ["add", "adc", "sub", "sbb"] and op1_family not in [None, 7] and "[" not in op1 and \
+               op2_family not in [None, 7] and "[" not in op2 and op1_family != op2_family:
+                self.practical_ASLR_ROP[10] = True
+                print("Instr: " + first_instr.raw + " satisfies practical ASLR ROP class [ADD|SDC|SUB|SBB] <GPR> <GPR>")
+
+        if self.practical_ASLR_ROP[11] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[12] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[13] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[14] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[15] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[16] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[17] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[18] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[19] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[20] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[21] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[22] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[23] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[24] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[25] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[26] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[27] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[28] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[29] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[30] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[31] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[32] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[33] is False:
+            #TODO
+            pass
+
+        if self.practical_ASLR_ROP[34] is False:
+            #TODO
+            pass
--- src/static_analyzer/Instruction.py
+++ src/static_analyzer/Instruction.py
@@ -14,22 +14,22 @@ class Instruction(object):
     The Instruction class represents a single instruction within a Gadget.
     """
 
-    register_families = [["rax", "eax", "ax", "al", "ah"],
-                         ["rbx", "ebx", "bx", "bl", "bh"],
-                         ["rcx", "ecx", "cx", "cl", "ch"],
-                         ["rdx", "edx", "dx", "dl", "dh"],
-                         ["rsi", "esi", "si", "sil"],
-                         ["rdi", "edi", "di", "dil"],
-                         ["rbp", "ebp", "bp", "bpl"],
-                         ["rsp", "esp", "sp", "spl"],
-                         ["r8", "r8d", "r8w", "r8b"],
-                         ["r9", "r9d", "r9w", "r9b"],
-                         ["r10", "r10d", "r10w", "r10b"],
-                         ["r11", "r11d", "r11w", "r11b"],
-                         ["r12", "r12d", "r12w", "r12b"],
-                         ["r13", "r13d", "r13w", "r13b"],
-                         ["r14", "r14d", "r14w", "r14b"],
-                         ["r15", "r15d", "r15w", "r15b"]]
+    register_families = [["rax", "eax", "ax", "al", "ah"],  # 0
+                         ["rbx", "ebx", "bx", "bl", "bh"],  # 1
+                         ["rcx", "ecx", "cx", "cl", "ch"],  # 2
+                         ["rdx", "edx", "dx", "dl", "dh"],  # 3
+                         ["rsi", "esi", "si", "sil"],       # 4
+                         ["rdi", "edi", "di", "dil"],       # 5
+                         ["rbp", "ebp", "bp", "bpl"],       # 6
+                         ["rsp", "esp", "sp", "spl"],       # 7
+                         ["r8", "r8d", "r8w", "r8b"],       # 8
+                         ["r9", "r9d", "r9w", "r9b"],       # 9
+                         ["r10", "r10d", "r10w", "r10b"],   # 10
+                         ["r11", "r11d", "r11w", "r11b"],   # 11
+                         ["r12", "r12d", "r12w", "r12b"],   # 12
+                         ["r13", "r13d", "r13w", "r13b"],   # 13
+                         ["r14", "r14d", "r14w", "r14b"],   # 14
+                         ["r15", "r15d", "r15w", "r15b"]]   # 15
 
     def __init__(self, raw_instr):
         """
